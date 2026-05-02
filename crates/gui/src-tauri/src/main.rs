7a033b4bf06a8f29319dd8c0c8927812f768da8f
fulatin <shijiahui2006hh@qq.com>
2026-05-02 20:30:33 +0800

    fix: 修复 set_var 在 Rust 2024 中需要 unsafe 块

--- crates/gui/src-tauri/src/main.rs
+++ crates/gui/src-tauri/src/main.rs
@@ -2,7 +2,9 @@ use estima_gui_lib::run;
 
 fn main() {
     // Fix Gdk-Message Error 71 on Wayland
-    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    unsafe {
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    }
     
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     run();
